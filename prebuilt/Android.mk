78492a2899bc06534b5a43e6aba11057929c8db2
sekaiacg <sekaiacg@gmail.com>
2023-01-12 14:17:42 +0800

    miui_prebuilt: Add depmod
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -4,10 +4,13 @@ include $(CLEAR_VARS)
     LOCAL_MODULE_TAGS := optional
     LOCAL_MODULE_CLASS := ETC
     LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+    LOCAL_HOST_REQUIRED_MODULES := depmod
     LOCAL_POST_INSTALL_CMD += \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules; \
         cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/modules $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/; \
         cp -rf $(LOCAL_PATH)/modules.load.recovery $(TARGET_RECOVERY_ROOT_OUT)/vendor/lib/modules/1.1/; \
         mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware; \
-        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/;
+        cp -rf $(LOCAL_PATH)/$(PRODUCT_RELEASE_NAME)/firmware $(TARGET_RECOVERY_ROOT_OUT)/vendor/; \
+        echo "calling depmod on miui_prebuilt"; \
+        $(DEPMOD) -b $(TARGET_RECOVERY_ROOT_OUT)/vendor 1.1;
 include $(BUILD_PHONY_PACKAGE)
